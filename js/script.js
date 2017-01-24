76ac0973bc52a06fcd31ffd79b8beff2d728e978
Jack304 <ddsolbadguy@gmail.com>
2017-01-24 21:51:13 +0300

    + appending input into div, centering conversation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 function sendInput() {
-    var x = document.getElementById("textMessage").value;
-    document.getElementById("demo").innerHTML = x;
+    var z = document.getElementById("textMessage").value;
+//    document.getElementById("demo").innerHTML = x;
+    var x = document.createElement("P");
+    var t = document.createTextNode(z);
+    x.appendChild(t);
+    document.getElementById("demo").appendChild(x);
 }
