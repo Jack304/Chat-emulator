1fbedca82e78f01fc55881a5aa24eede15cb01c6
Jack304 <ddsolbadguy@gmail.com>
2017-02-01 23:18:50 +0300

    closed #2 replaced <p> with <div>

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var messageIdNumber = 0;
 function sendMessage() {
     var input = document.getElementById("text_message").value;
-    var createMessage = document.createElement("P");
+    var createMessage = document.createElement("div");
     createMessage.textContent = input;
     createMessage.id = "message_id_" + messageIdNumber;
     document.getElementById("message_area_id_1").appendChild(createMessage);
