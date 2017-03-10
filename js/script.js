68023f2aeee4eddf3a63dc0ad802c2832e2749a4
Jack304 <ddsolbadguy@gmail.com>
2017-03-10 17:08:50 +0300

    closed #8

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function sendMessage() {
     var createMessage = document.createElement("div");
     createMessage.textContent = input;
     createMessage.id = "message_id_" + messageIdNumber;
+    createMessage.className = "message";
     document.getElementById("message_area_id_1").appendChild(createMessage);
     messageIdNumber++;
 }
